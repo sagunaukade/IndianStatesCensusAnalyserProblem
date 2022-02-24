232bc9d4b8520b9e3b34041ea6953c9635ace7d4
sagunaukade <sagunaukade96@gmail.com>
2022-02-24 16:43:58 +0530

    [Saguna] Add Indian Census Project

--- IndianStatesAnalyserProblems/DTO/Program.cs
+++ IndianStatesAnalyserProblems/DTO/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IndianStatesCensusAnalyser
+namespace DTO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Indian States Census Analyser Problem");
-            Console.ReadLine();
         }
     }
 }
---
+++ IndianStatesAnalyserProblems/DTO/StateCodeDAO.cs
@@ -0,0 +1,10 @@
+﻿namespace DTO
+{
+    public class StateCodeDAO
+    {
+        internal int serialNumber;
+        internal string stateName;
+        internal int tin;
+        internal string stateCode;
+    }
+}
